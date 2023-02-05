d98333e119b75b335091d980a76e37605729b3a4
Arlen Molina <s3iyadragon@gmail.com>
2023-02-05 09:40:09 -0600

    0.0.7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 export default {
-  buildModules: ['@nuxt/typescript-build'],
-  modules: ['nuxt-icon']
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/google-analytics'],
+  modules: ['nuxt-icon'],
+  googleAnalytics: {
+    id: 'G-EZTSQPPMFJ'
+  },
 };
